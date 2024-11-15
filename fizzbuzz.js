e63aab47efcee24099dbb6a463341b7f2b3c6724
moman01 <learnermoman@gmail.com>
2024-11-15 21:54:55 +0600

    fizzbuzz solved

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,3 @@
-
 // Problem: FizzBuzz
 // Write a function called fizzBuzz that takes a number as input and logs numbers from 1 to that number to the console with the following rules:
 
@@ -7,8 +6,58 @@
 // For numbers divisible by both 3 and 5, print "FizzBuzz".
 // For all other numbers, print the number itself.
 
+// Example Output
+// If the input is 15, the output should be:
 
+// 1;
+// 2;
+// Fizz;
+// 4;
+// Buzz;
+// Fizz;
+// 7;
+// 8;
+// Fizz;
+// Buzz;
+// 11;
+// Fizz;
+// 13;
+// 14;
+// FizzBuzz;
 
-function fizzbuzz(num){
+function fizzBuzz(num) {
+  for (let i = 1; i <= num; i++) {
+
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("FIzz Buzz");
+      }
+      else{
+        console.log(i);
+      }
+  }
 
 }
+fizzBuzz(15);
+
+
+
+// Chatgpt Solutions
+
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         console.log(
+//             (i % 3 === 0 && i % 5 === 0) ? "FizzBuzz" :
+//             (i % 3 === 0) ? "Fizz" :
+//             (i % 5 === 0) ? "Buzz" :
+//             i
+//         );
+//     }
+// }
+
+//  Test the function
+// fizzBuzz(15);
+
