b0ceb8b3b6a2a1ca86ebfd9ae13948c0a44d3cf9
moman01 <learnermoman@gmail.com>
2024-11-15 21:18:14 +0600

    first problem added

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+
+// Problem: FizzBuzz
+// Write a function called fizzBuzz that takes a number as input and logs numbers from 1 to that number to the console with the following rules:
+
+// For numbers divisible by 3, print "Fizz".
+// For numbers divisible by 5, print "Buzz".
+// For numbers divisible by both 3 and 5, print "FizzBuzz".
+// For all other numbers, print the number itself.
+
+
+
+function fizzbuzz(num){
+
+}
